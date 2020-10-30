40582d07f07d1b473746c5bd66999b0a6048d837
christopherhendrich <50594177+christopherhendrich@users.noreply.github.com>
2020-10-30 16:27:24 -0700

    Update remote-node-prep.sh

--- remote-node-prep.sh
+++ remote-node-prep.sh
@@ -2,9 +2,3 @@ sudo setenforce 0
 sudo sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 sudo yum install -y yum-utils
 sudo systemctl disable firewalld --now
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl enable docker --now
-sudo usermod -aG docker $USER
