0e3242307af607e06fa6bee18de1205f57848111
christopherhendrich <50594177+christopherhendrich@users.noreply.github.com>
2020-10-30 19:31:25 -0700

    Update remote-node-prep.sh

--- remote-node-prep.sh
+++ remote-node-prep.sh
@@ -2,3 +2,4 @@ sudo setenforce 0
 sudo sed -i -e 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 sudo yum install -y yum-utils
 sudo systemctl disable firewalld --now
+sudo yum install iproute-tc
